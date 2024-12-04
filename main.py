c7bbe23ab9cf60a1f994b79060ba433de25762e9
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-04 23:24:17 +0000

    up diagonal check and updated test

--- main.py
+++ main.py
@@ -44,6 +44,23 @@ def down_diagonal(row, col, lstr):
   for i in range(0,4):
     test += lsub[i][col+i]
   return is_match(test)
+
+def up_diagonal(row, col, lstr):
+  print(lstr)
+  if row < 3:
+    print('rows')
+    return False
+  lsub = lstr[row-3:row+1]
+  print(lsub)
+  for x in lsub:
+    if len(x) < col + 4:
+      print(f'cols: {x}')
+      return False
+  test = ''
+  for i in range(0,4):
+    print(f' {3-i},{col+i}')
+    test += lsub[3-i][col+i]
+  return is_match(test)
   
 print('Hello world!')
 tests = [['1X11',
@@ -66,8 +83,11 @@ inputs = readlines('test.txt')
 #print(vertical(0,1,tests[0]))
 #print(vertical(0,1,tests[1]))
 #print(vertical(1,1,tests[1]))
-print(down_diagonal(0,0, tests[0]))
-print(down_diagonal(1,1, tests[2]))
+#print(down_diagonal(0,0, tests[0]))
+#print(down_diagonal(1,1, tests[2]))
+print(up_diagonal(1,1, tests[2]))
+print(up_diagonal(3,1, tests[2]))
+print(up_diagonal(3,1, inputs))
 #for line in inputs:
 #  for i in range(0, len(line)-3):
 #    print(horizontal(i, line))
