cdd27590f19082b74ce82d440d75fa20e0a0f77b
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-05 23:00:26 +0000

    Finished part 2

--- advent5.py
+++ advent5.py
@@ -30,12 +30,12 @@ def extract_rules_and_updates(lines):
       updates.append(update)
 
   # Remove overlapping rules
-  
+
   for key, values in increasing_rules.items():
     values.sort()
   for key, values in decreasing_rules.items():
     values.sort(reverse=True)
-    
+
   return increasing_rules, decreasing_rules, updates
 
 
@@ -43,9 +43,11 @@ def debug_rules(rules, is_increasing=True):
   for key in rules:
     print(f'Rule: {key}: {rules[key]}')
 
+
 def debug_updates(updates):
   for update in updates:
     print(f'Update: {update}')
+
 
 def has_decr_match(page1, page2, rules):
   if page2 > page1:
@@ -54,7 +56,8 @@ def has_decr_match(page1, page2, rules):
     return False
   valuesl = rules[page1]
   return page2 in valuesl
-  
+
+
 def has_incr_match(page1, page2, rules):
   if page2 < page1:
     return False
@@ -63,10 +66,12 @@ def has_incr_match(page1, page2, rules):
   valuesl = rules[page1]
   return page2 in valuesl
 
+
 def swap(i, j, arr):
   tmp = arr[j]
   arr[j] = arr[i]
   arr[i] = tmp
+
 
 ####
 # Main
@@ -75,29 +80,23 @@ inputs = readlines('input5.txt')
 
 increasing_rules, decreasing_rules, updates = extract_rules_and_updates(inputs)
 
-#print('Increasing Rules:')
-#debug_rules(increasing_rules)
-#print('Decreasing Rules:')
-#debug_rules(decreasing_rules, is_increasing=False)
-
 total = 0
 for update in updates:
   has_violation = False
   orig = update.copy()
   for i in range(0, len(update)):
-    for j in range(i+1, len(update)):
+    for j in range(i + 1, len(update)):
       if has_incr_match(update[j], update[i], increasing_rules):
-        #print(f' Rule violation: {update[i]} -> {update[j]}')
         has_violation = True
-        swap(i,j, update)
+        swap(i, j, update)
+        j = i + 1
       elif has_decr_match(update[j], update[i], decreasing_rules):
-        #print(f' Rule decr violation: {update[i]} -> {update[j]}')
         has_violation = True
-        swap(i,j,update)
-  #if has_violation:
-  #  print(f' {orig}: {update}')
+        swap(i, j, update)
+        j = i + 1
   mid = int(len(update) / 2)
-  total += update[mid]
-    
+  if has_violation:
+    print(f' mid: {update[mid]}')
+    total += update[mid]
 
 print(f'Total: {total}')
