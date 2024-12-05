212a80e175c7c1ce6a6b95667a7b9958b4f6a1e0
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-05 18:54:57 +0000

    rename and save data for day 4

--- advent4.py
+++ advent4.py
@@ -8,66 +8,70 @@ def readlines(source):
 
 
 def is_match(str):
-  result = (str == 'XMAS' or str == 'SAMX')
+  result = (str == 'MAS' or str == 'SAM')
   return result
 
 
 def horizontal(start, str):
-  if len(str) < 4:
+  if len(str) < 3:
     return False
-  sub = str[start:start + 4]
+  sub = str[start:start + 3]
   return is_match(sub)
 
 
 def vertical(row, col, lstr):
   lsub = lstr[row:]
-  if len(lsub) < 4:
+  if len(lsub) < 3:
     return False
   for x in lsub:
     if len(x) < col + 1:
       return False
   test = ''
-  for i in range(0, 4):
+  for i in range(0, 3):
     test += lsub[i][col]
   return is_match(test)
 
 
 def down_diagonal(row, col, lstr):
-  lsub = lstr[row:row + 4]
-  if len(lsub) < 4:
+  lsub = lstr[row:row + 3]
+  if len(lsub) < 3:
     return False
   for x in lsub:
-    if len(x) < col + 4:
+    if len(x) < col + 3:
       return False
   test = ''
-  for i in range(0, 4):
+  for i in range(0, 3):
     test += lsub[i][col + i]
   return is_match(test)
 
 
 def up_diagonal(row, col, lstr):
-  if row < 3:
+  if row < 2:
     return False
-  lsub = lstr[row - 3:row + 1]
+  lsub = lstr[row - 2:row + 1]
   for x in lsub:
-    if len(x) < col + 4:
+    if len(x) < col + 3:
       return False
   test = ''
-  for i in range(0, 4):
-    test += lsub[3 - i][col + i]
+  for i in range(0, 3):
+    test += lsub[2 - i][col + i]
   return is_match(test)
 
-inputs = readlines('input4.txt')
+
+inputs = readlines('input4-2.txt')
+
+def check_x_formation(row, column, board):
+  if len(board) < row + 3:  # Not enough rows for cross
+    return False
+  if len(board[row]) < column + 3:  # Not enough columns for cross
+    return False
+  return down_diagonal(row, column, inputs) and up_diagonal(
+      row + 2, column, inputs)
+
 
 total = 0
 for row in range(0, len(inputs)):
   for column in range(0, len(inputs[row])):
-    if horizontal(column, inputs[row]):
-      total += 1
-    if vertical(row, column, inputs):
-      total += 1
-    if down_diagonal(row, column, inputs):
-      total += 1
-    if up_diagonal(row, column, inputs):
+    if check_x_formation(row, column, inputs):
       total += 1
 print(f'Total: {total}')
