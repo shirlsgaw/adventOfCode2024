95ee0fc0881f77f3d4fabd1843a0901d900044c7
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-06 22:01:54 +0000

    Keep moving until the guard is in a viable location

--- advent6.py
+++ advent6.py
@@ -19,26 +19,34 @@ class GuardLocation:
     return f'Guard at {self.x}, {self.y} facing {self.direction}'
 
   def move(self, lab_map) -> Self:
-    if self.direction == Direction.NORTH:
-      if has_block(self.x, self.y + 1, lab_map):
-        self.direction = Direction.EAST
+    has_moved = False
+    while not has_moved and not is_outside(self.x, self.y, lab_map):
+      print(self)
+      if self.direction == Direction.NORTH:
+        if has_block(self.x, self.y + 1, lab_map):
+          self.direction = Direction.EAST
+        else:
+          self.y += 1
+          has_moved = True
+      elif self.direction == Direction.EAST:
+        if has_block(self.x + 1, self.y, lab_map):
+          self.direction = Direction.SOUTH
+        else:
+          self.x += 1
+          has_moved = True
+      elif self.direction == Direction.SOUTH:
+        if has_block(self.x, self.y - 1, lab_map):
+          self.direction = Direction.WEST
+        else:
+          self.y -= 1
+          has_moved = True
       else:
-        self.y += 1
-    elif self.direction == Direction.EAST:
-      if has_block(self.x + 1, self.y, lab_map):
-        self.direction = Direction.SOUTH
-      else:
-        self.x += 1
-    elif self.direction == Direction.SOUTH:
-      if has_block(self.x, self.y - 1, lab_map):
-        self.direction = Direction.WEST
-      else:
-        self.y -= 1
-    else:
-      if has_block(self.x - 1, self.y, lab_map):
-        self.direction = Direction.NORTH
-      else:
-        self.x -= 1
+        if has_block(self.x - 1, self.y, lab_map):
+          self.direction = Direction.NORTH
+        else:
+          self.x -= 1
+          has_moved = True
+      
     return self
 
 def readlines(source):
@@ -65,35 +73,31 @@ def find_guard(lab_map):
 
 
 def has_block(x, y, lab_map):
+  if is_outside(x, y, lab_map):
+    return False
   row = lab_map[y]
-  print(f'row {row}')
+  print(f' row {row}')
   return y < len(row) and row[x] == '#'
 
-def is_outside(location, lab_map):
-  return location.x < 0 or location.x >= len(lab_map[0]) or location.y  < 0 or location.y >= len(lab_map)
+def is_outside(x, y, lab_map):
+  return x < 0 or x >= len(lab_map[0]) or y  < 0 or y >= len(lab_map)
 
 ####
 # Main
 ####
-inputs = ['.>.']
-print(f'Guard: {find_guard(inputs)}')
-inputs = ['.v.']
-print(f'Guard: {find_guard(inputs)}')
-inputs = ['.<.']
-print(f'Guard: {find_guard(inputs)}')
-
-
 inputs = ['....#.....', '.#..^.....']  #readlines('input5.txt')
-print(f'Guard: {find_guard(inputs)}')
-
-test = has_block(1, 1, ['..', '.#'])
-print(f'Has block: {test}')
-test = has_block(0, 0, ['..', '.#'])
-print(f'Has block: {test}')
 
 guard = GuardLocation(0, 0, Direction.EAST).move(['..'])
 print(f'New guard: {guard}')
+test = is_outside(guard.x, guard.y, ['..#'])
+print(f'  Is outside: {test}')
+test = is_outside(guard.x, guard.y, ['.'])
+print(f'  Is outside: {test}')
 guard = guard.move(['..#'])
 print(f'New guard: {guard}')
+test = is_outside(guard.x, guard.y, ['.'])
+print(f'  Is outside: {test}')
+
 guard = GuardLocation(1, 0, Direction.WEST).move(['#.'])
 print(f'New guard: {guard}')
+
