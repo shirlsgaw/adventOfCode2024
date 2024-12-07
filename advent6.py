ffa969dd3bf6595e8c8d784253527b58fccbf331
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-07 00:25:16 +0000

    Extract function for lookahead of location

--- advent6.py
+++ advent6.py
@@ -18,36 +18,44 @@ class GuardLocation:
   def __str__(self) -> str:
     return f'Guard at {self.x}, {self.y} facing {self.direction}'
 
-  def move(self, lab_map: list[str]) -> Self:
-    has_moved = False
-    while not has_moved and not is_outside(self.x, self.y, lab_map):
-      #print(self)
-      if self.direction == Direction.NORTH:
-        if has_block(self.x, self.y - 1, lab_map):
-          self.direction = Direction.EAST
-        else:
-          self.y -= 1
-          has_moved = True
-      elif self.direction == Direction.EAST:
-        if has_block(self.x + 1, self.y, lab_map):
-          self.direction = Direction.SOUTH
-        else:
-          self.x += 1
-          has_moved = True
-      elif self.direction == Direction.SOUTH:
-        if has_block(self.x, self.y + 1, lab_map):
-          self.direction = Direction.WEST
-        else:
-          self.y += 1
-          has_moved = True
-      else:
-        if has_block(self.x - 1, self.y, lab_map):
-          self.direction = Direction.NORTH
-        else:
-          self.x -= 1
-          has_moved = True
+  def copy(self):
+    return GuardLocation(self.x, self.y, self.direction)
 
+  def move(self, lab_map: list[str]) -> Self:
+    next_location = get_next_move(self, lab_map)
+    self.x = next_location.x
+    self.y = next_location.y
+    self.direction = next_location.direction
     return self
+
+
+def get_next_move(location: GuardLocation,
+                  lab_map: list[str]) -> GuardLocation:
+  x = location.x
+  y = location.y
+  direction = location.direction
+  while not is_outside(x, y, lab_map):
+    if direction == Direction.NORTH:
+      if has_block(x, y - 1, lab_map):
+        direction = Direction.EAST
+      else:
+        return GuardLocation(x, y - 1, direction)
+    elif direction == Direction.EAST:
+      if has_block(x + 1, y, lab_map):
+        direction = Direction.SOUTH
+      else:
+        return GuardLocation(x + 1, y, direction)
+    elif direction == Direction.SOUTH:
+      if has_block(x, y + 1, lab_map):
+        direction = Direction.WEST
+      else:
+        return GuardLocation(x, y + 1, direction)
+    else:
+      if has_block(x - 1, y, lab_map):
+        direction = Direction.NORTH
+      else:
+        return GuardLocation(x - 1, y, direction)
+  return GuardLocation(x, y, direction)
 
 
 def readlines(source: str) -> list[str]:
@@ -127,6 +135,8 @@ def count_unique_visits(lab_map: list[str]) -> int:
 inputs = readlines('sample.txt')
 original_input = inputs.copy()
 guard = find_guard(inputs)
+original_guard = GuardLocation(guard.x, guard.y, guard.direction)
+
 if guard is None:
   print('No guard found')
   exit()
@@ -141,3 +151,18 @@ print(f'Final guard at {guard.x}, {guard.y} facing {guard.direction}')
 count = count_unique_visits(inputs)
 print(f'Part 1: {count}')
 
+inputs = original_input.copy()
+guard = GuardLocation(original_guard.x, original_guard.y,
+                      original_guard.direction)
+mark_guard(guard, inputs)
+
+guard = move_guard_max_steps(guard, inputs, 3)
+if guard is None:
+  print('Ended moves with no guard')
+  exit()
+print(f'Final guard at {guard.x}, {guard.y} facing {guard.direction}')
+debug_map(inputs)
+count = count_unique_visits(inputs)
+print(
+    f'Part 2: {count}, guard location ({guard})character: {inputs[guard.y][guard.x]}'
+)
