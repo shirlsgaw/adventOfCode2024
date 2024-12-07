4ddc3b3ef816370ee8d479202701b60b6e19aa1c
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-07 20:22:49 +0000

    Check previous value (-, |, +) where the guard hit the obstruction

--- advent6.py
+++ advent6.py
@@ -137,28 +137,47 @@ def is_valid_obstruction(obstruction: tuple[int,int],
                          lab_map: list[str],
                          max_steps: int
                         ) -> bool:
-  #mark_guard(guard, inputs)
   mark_guard(GuardLocation(obstruction[0], obstruction[1], Direction.NORTH), 
              inputs, 
              mark_char='O')  # Add test barrier in original path
   obstruction_hits = 0
   replay_steps = 0
+  collision_location = None
+  steps_to_hit = max_steps
   while obstruction_hits < 2 and not is_outside(guard.x, guard.y,
                                             inputs) and replay_steps < max_steps:
     (next, found_obstruction) = get_next_move(guard, inputs)    
     if found_obstruction:
+      collision_location = (guard.x, guard.y)
       obstruction_hits += 1
+    if guard.direction != next.direction:
+      mark_guard(guard, inputs, mark_char='+')
+    elif guard.direction in {Direction.EAST, Direction.WEST}:
+      mark_guard(guard, inputs, mark_char='-')
+    else:
+      mark_guard(guard, inputs, mark_char='|')
     guard = next
     replay_steps += 1
-  mark_guard(GuardLocation(obstruction[0], obstruction[1], Direction.NORTH), 
-     inputs, 
-     mark_char='.')  # Reset
-  return obstruction_hits >= 2
+  if is_outside(guard.x, guard.y, inputs):
+    return False
+  if collision_location is None:
+    return False
+  obstruction_str= inputs[collision_location[1]][collision_location[0]]
+  print(f'Obstruction: {obstruction_str} at {collision_location}')
+  return obstruction_str == '+'
+    
+  #if obstruction_hits >= 2:
+  #  return True
+  #if obstruction_hits == 0:
+  #  return False
+  #if steps_to_hit != max_steps:
+  #  print(f'Obstruction {obstruction} hit {obstruction_hits} in {steps_to_hit} steps')
+  #return False
 
 ####
 # Main
 ####
-inputs = readlines('input6.txt')
+inputs = readlines('hint.txt')
 original_input = inputs.copy()
 guard = find_guard(inputs)
 original_guard = GuardLocation(guard.x, guard.y, guard.direction)
@@ -167,10 +186,10 @@ mark_guard(guard, inputs)
 
 (guard, orig_steps) = move_guard_max_steps(guard, inputs,
                                            len(inputs) * len(inputs[0]))
-print(f'Final guard at {guard.x}, {guard.y} facing {guard.direction}')
+#print(f'Final guard at {guard.x}, {guard.y} facing {guard.direction}')
 count = count_unique_visits(inputs)
 print(f'Part 1: count={count}, steps={orig_steps}')
-#debug_map(inputs)
+debug_map(inputs)
 obstructions = set()
 candidates = list()
 
@@ -182,11 +201,19 @@ for row_index in range(0, len(inputs)):
     if inputs[row_index][column_index] == 'X':
       candidates.append((column_index, row_index))
 inputs = original_input.copy()
+
+# Test all candidates
+#candidates = list()
+#candidates.append((118, 117)) # Obstruction (118, 11) hit 1 in 4648 steps
+#candidates.append((121, 17)) # Obstruction (121, 17) hit 1 in 4657 steps
+#candidates.append((123, 17)) # Obstruction (123, 17) hit 1 in 4747 steps
+#candidates.append((115, 18)) # Obstruction (115, 18) hit 1 in 3420 steps
 for (cadidate_x, candidate_y) in candidates:
   guard = GuardLocation(original_guard.x, original_guard.y,
                         original_guard.direction)
+  inputs = original_input.copy()
   if is_valid_obstruction((cadidate_x, candidate_y), 
-                          guard, inputs, orig_steps * 2):
+                          guard, inputs, len(inputs) * len(inputs[0])):
     obstructions.add((cadidate_x, candidate_y))
-      
+  debug_map(inputs)    
 print(f'Part 2: count={len(obstructions)}')
