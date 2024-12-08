7a9e9d1a7ef5a51b451242e15b2354216b0c89a0
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-08 02:48:47 +0000

    Part 1: success sample

--- advent7.py
+++ advent7.py
@@ -6,6 +6,35 @@ def readlines(source):
     lines = f.readlines()
     return list(map(lambda x: x.rstrip(), lines))
 
+
+def find_operations(total, values):
+  if len(values) < 2:
+    raise AssertionError(f'Not enough values: {values}')
+  # Base case
+  if len(values) == 2:
+    if total == values[0] + values[1]:
+      return f'{values[0]}+{values[1]}'
+    if total == values[0] * values[1]:
+      return f'{values[0]}*{values[1]}'
+    return None
+  else:
+    last = values.pop()
+    sub_total = total - last
+    div_total = total / last
+    if sub_total >= 0:
+      result = find_operations(sub_total, values)
+      #print(f' recurse add {total}={result}+{last}')
+      if result is not None:
+        return f'{result}+{last}'
+    if total % last == 0:
+      result = find_operations(div_total, values)
+      #print(f' recurse multiply {total}={result}*{last}')
+      if result is None:
+        return None
+      return f'{result}*{last}'
+    return None
+
+
 ###
 # Main
 ###
@@ -17,10 +46,29 @@ for row in inputs:
   totals_list.append(int(str1))
   values = str2.split(' ')
   operands = list()
-  for v in values: 
+  for v in values:
     if v.isnumeric():
       operands.append(int(v))
   operands_list.append(operands)
-print(totals_list)
-print(operands_list)
-print('Day 7')
+
+# Test base cases
+#print(find_operations(190, [10, 19]))
+#print(find_operations(100, [10, 19]))
+#print(find_operations(125, [100, 25]))
+
+# Test recursive cases
+#print(find_operations(3267, [81, 40, 27]))
+#print(find_operations(464, [12, 34, 56]))
+#print(find_operations(7290, [6, 8, 6, 15]))
+#print(find_operations(292, [11,6,16,20]))
+#print(find_operations(272, [11, 6, 16]))
+
+part1_total = 0
+for i in range(0, len(totals_list)):
+  result = find_operations(totals_list[i], operands_list[i])
+  if result is None:
+    print(f' Failed for {totals_list[i]}')
+  else:
+    print(f' Success: {totals_list[i]} = {result}')
+    part1_total += totals_list[i]
+print(f'Part 1: {part1_total}')
