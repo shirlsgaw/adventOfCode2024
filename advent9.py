114b82f3fcf49a9566f8160c023511150c42aae9
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-10 00:47:42 +0000

    Day 9, Part 2: implement searching and switching equal sized blocked and space blocks

--- advent9.py
+++ advent9.py
@@ -29,6 +29,29 @@ def expand(input: str) -> list[str]:
   return result
 
 
+def expand2(input: str) -> list[tuple[int, str]]:
+  file_id = 0
+  result = list[tuple[int, str]]()
+  index = 0
+  while index < len(input):
+    reps = input[index]
+    block = ''
+    for r in range(0, int(reps)):
+      block += str(file_id)
+    result.append((int(reps), block))
+    file_id += 1
+
+    index += 1
+    if index < len(input):
+      spaces = input[index]
+      space_block = ''
+      for s in range(0, int(spaces)):
+        space_block += '.'
+      result.append((int(spaces), space_block))
+      index += 1
+  return result
+
+
 def compact(input: list[str]) -> list[str]:
   result = input.copy()
   i = 0
@@ -46,6 +69,41 @@ def compact(input: list[str]) -> list[str]:
   return result
 
 
+def first_space_index(target_length: int, input: list[tuple[int, str]]) -> int:
+  index = 0
+  while index < len(input):
+    (tmp_length, tmp_block) = input[index]
+    if tmp_block.startswith('.') and tmp_length == target_length:
+      return index
+    index += 1
+  return -1
+
+
+def first_block_index(start_at: int, input: list[tuple[int, str]]) -> int:
+  index = start_at
+  while index >= 0:
+    (tmp_length, tmp_block) = input[index]
+    if not tmp_block.startswith('.'):
+      return index
+    index -= 1
+  return index
+
+
+def compact2(input: list[tuple[int, str]]):
+  block_index = first_block_index(len(input) - 1, input)
+  while block_index >= 0:
+    (tmp_length, tmp_block) = input[block_index]
+    space_index = first_space_index(tmp_length, input)
+    if space_index < block_index:
+      tmp = input[space_index]
+      input[space_index] = input[block_index]
+      input[block_index] = tmp
+
+    # Move to the next block
+    block_index -= 1
+    block_index = first_block_index(block_index, input)
+
+
 def generate_check_sum(input: list[str]) -> int:
   check_sum = 0
   for i in range(0, len(input)):
@@ -61,10 +119,23 @@ disk_map = readlines('input9.txt')
 
 disk = expand(disk_map[0])
 pretty = ''.join(disk)
-#print(pretty)
 
 disk = compact(disk)
-#print(disk)
-
 check_sum = generate_check_sum(disk)
 print(check_sum)
+
+disk2 = expand2(disk_map[0])
+output = ''
+for (_, block) in disk2:
+  output += block
+#print(f'Part 1 expand equals part 2: {pretty == output}')
+
+disk2 = [(3, 'aaa'), (5, '.....'), (5, 'bbbbb'), (1, 'a'), (3, '...')]
+print(first_block_index(len(disk2) - 1, disk2))
+print(first_block_index(2, disk2))
+print(first_block_index(1, disk2))
+
+print(first_space_index(5, disk2))
+print(first_space_index(3, disk2))
+compact2(disk2)
+print(disk2)
