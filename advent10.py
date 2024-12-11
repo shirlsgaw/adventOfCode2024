aa58473cb07b68331264aa65ae88d6bededb7bb9
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-11 02:06:36 +0000

    Day 10, Part 2: small sample ok

--- advent10.py
+++ advent10.py
@@ -32,15 +32,17 @@ def is_outside(x: int, y: int, location_map: list[str]):
 ####
 def is_valid_location(curr_x: int, curr_y: int, next_x: int, next_y: int,
                       location_map: list[str],
-                      visited: set[tuple[int, int]]) -> bool:
+                      visited: set[tuple[int, int]],
+                      check_visited: bool
+                     ) -> bool:
   if is_outside(next_x, next_y, location_map):
     return False
 
   curr_value = location_map[curr_y][curr_x]
   next_value = location_map[next_y][next_x]
   
-  if (next_x, next_y) in visited:
-    return False
+  #if check_visited and (next_x, next_y) in visited:
+  #  return False
 
   expected = str(int(curr_value) + 1)
   return next_value == expected
@@ -51,33 +53,33 @@ def is_valid_location(curr_x: int, curr_y: int, next_x: int, next_y: int,
 ####
 def get_horizon(
     x: int, y: int, map: list[str],
-    visited: set[tuple[int, int]]) -> Generator[tuple[int, int], None, None]:
+    visited: set[tuple[int, int]], check_visited=True) -> Generator[tuple[int, int], None, None]:
   # North
   #print(f'. North of ({x}, {y})')
   next_y = y - 1
   next_x = x
-  if is_valid_location(x, y, next_x, next_y, map, visited):
+  if is_valid_location(x, y, next_x, next_y, map, visited, check_visited):
     yield (next_x, next_y)
 
   # South
   #print(f'. South of ({x}, {y})')
   next_y = y + 1
   next_x = x
-  if is_valid_location(x, y, next_x, next_y, map, visited):
+  if is_valid_location(x, y, next_x, next_y, map, visited, check_visited):
     yield (next_x, next_y)
 
   # East
   #print(f'. East of ({x}, {y})')
   next_y = y
   next_x = x + 1
-  if is_valid_location(x, y, next_x, next_y, map, visited):
+  if is_valid_location(x, y, next_x, next_y, map, visited, check_visited):
     yield (next_x, next_y)
 
   # West
   #print(f'. West of ({x}, {y})')
   next_y = y
   next_x = x - 1
-  if is_valid_location(x, y, next_x, next_y, map, visited):
+  if is_valid_location(x, y, next_x, next_y, map, visited, check_visited):
     yield (next_x, next_y)
 
   return None
@@ -85,7 +87,7 @@ def get_horizon(
 ####
 # Main
 ####
-topo_map = readlines('input10.txt')
+topo_map = readlines('hint.txt')
 
 
 sum = 0
@@ -93,6 +95,7 @@ sum = 0
 for (x, y) in find_trailheads(topo_map):
   nine_locations = set[tuple[int, int]]()
   visited = set[tuple[int, int]]()
+  count = 0
 
   unexplored = list[tuple[int, int]]()
   #print(f'Trailhead: ({x}, {y})')
@@ -101,17 +104,18 @@ for (x, y) in find_trailheads(topo_map):
   while len(unexplored) > 0:
     curr_x, curr_y = unexplored.pop(0)
     visited.add((curr_x, curr_y))
-    #print(f'({curr_x}, {curr_y}): {topo_map[curr_y][curr_x]}, unexplored: {unexplored}')
+    #print(f'({curr_x}, {curr_y}): {topo_map[curr_y][curr_x]}')
     
 
     if topo_map[curr_y][curr_x] == '9':
-      nine_locations.add((curr_x, curr_y))
+      count += 1
+      
       #print(f'Found a 9 at ({curr_x}, {curr_y}), locations={nine_locations}')
     else:
       # Start path finding to 9
-      for hx, hy in get_horizon(curr_x, curr_y, topo_map, visited):
+      for hx, hy in get_horizon(curr_x, curr_y, topo_map, visited, check_visited=False):
         unexplored.append((hx, hy))
-        #print(f'...Horizon ({curr_x}, {curr_y}): ({hx}, {hy})')
-  print(f'Trailhead: ({x}, {y}): {len(nine_locations)}')
-  sum += len(nine_locations)
-print('Part 1: ' + str(sum))
+        #print(f'...Horizon ({curr_x}, {curr_y}): {topo_map[curr_y][curr_x]} -> ({hx}, {hy})')
+  print(f'Trailhead: ({x}, {y}): {count}')
+  sum += count
+print('Part 2: ' + str(sum))
