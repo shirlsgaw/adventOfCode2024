7008c53a3ca6ad00941394260b19edc9cc4c9052
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-13 21:41:52 +0000

    Clean up to use regular expressions and better iteration on parsing input

--- advent13.py
+++ advent13.py
@@ -1,5 +1,7 @@
 from collections.abc import Generator
 from typing import Union
+from scipy import optimize
+import re
 
 
 ####
@@ -7,18 +9,10 @@ from typing import Union
 ####
 class Button:
 
-  def __init__(self, label: str, x_label: str, y_label: str):
-    print(label)
-    index = label.index(':')
-    self.label = label[:index]
-
-    index = x_label.index(',')
-    x_label = x_label[:index]
-    tmp = x_label.split('+')
-    self.x = int(tmp[1])
-
-    tmp = y_label.split('+')
-    self.y = int(tmp[1])
+  def __init__(self, label: str, x: int, y: int):
+    self.label = label
+    self.x = x
+    self.y = y
 
     if self.label == 'A':
       self.cost = 3
@@ -41,15 +35,9 @@ class Button:
 ####
 class Prize:
 
-  def __init__(self, x_label: str, y_label: str):
-
-    index = x_label.index(',')
-    x_label = x_label[:index]
-    tmp = x_label.split('=')
-    self.x = int(tmp[1])
-
-    tmp = y_label.split('=')
-    self.y = int(tmp[1])
+  def __init__(self, x: int, y: int):
+    self.x = x
+    self.y = y
 
   def __repr__(self):
     return f"Prize(({self.x}, {self.y}))"
@@ -87,11 +75,15 @@ def readlines(source):
 # Parses a string into Button or Prize
 ####
 def parse(line: str) -> Union[Button, Prize, None]:
-  tmp = line.split(' ')
-  if tmp[0].startswith('Button'):
-    return Button(label=tmp[1], x_label=tmp[2], y_label=tmp[3])
-  if tmp[0].startswith('Prize'):
-    return Prize(x_label=tmp[1], y_label=tmp[2])
+  matches = re.findall(r'Button (A|B): X\+(\d+), Y\+(\d+)', line)
+  if len(matches) > 0:
+    match = matches[0]
+    return Button(label=match[0], x=int(match[1]), y=int(match[2]))
+
+  matches = re.findall(r'Prize: X\=(\d+), Y\=(\d+)', line)
+  if len(matches) > 0:
+    match = matches[0]
+    return Prize(x=int(match[0]), y=int(match[1]))
   return None
 
 
@@ -104,24 +96,24 @@ button1 = None
 button2 = None
 prize = None
 games = list[Game]()
-for line in input:
-  result = parse(line)
-  if result is None and button1 is not None and button2 is not None and prize is not None:
+
+index = 0
+while index < len(input):
+  line = input[index]
+  if len(line) == 0:
+    index += 1
+  elif index + 2 < len(input):
+    button1 = parse(input[index])
+    if not isinstance(button1, Button):
+      raise Exception(f"Expected button at index {index}")
+    button2 = parse(input[index + 1])
+    if not isinstance(button2, Button):
+      raise Exception(f"Expected button at index {index + 1}")
+    prize = parse(input[index + 2])
+    if not isinstance(prize, Prize):
+      raise Exception(f"Expected prize at index {index + 2}")
     game = Game(buttons=(button1, button2), prize=prize)
     games.append(game)
-    button1 = None
-    button2 = None
-    prize = None
-  if isinstance(result, Button):
-    if button1 is None:
-      button1 = result
-    else:
-      button2 = result
-  if isinstance(result, Prize):
-    prize = result
+    index += 3
 
-# The last game has no blank line divider
-if button1 is not None and button2 is not None and prize is not None:
-  game = Game(buttons=(button1, button2), prize=prize)
-  games.append(game)
 print(games)
