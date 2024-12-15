c6d75464dab93748b57146a0a3bd393b39fded95
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-15 22:03:37 +0000

    Working small example for Part 1

--- advent15.py
+++ advent15.py
@@ -37,6 +37,7 @@ class Warehouse:
   # Draw the map
   ####
   def draw(self):
+    #print(f'Robot expected location: {self.robot}')
     for y in range(0, len(self.original)):
       row = ''
       for x in range(0, len(self.original[y])):
@@ -52,6 +53,41 @@ class Warehouse:
           row += '.'
       print(row)
 
+  ####
+  # Simulate the robot moving
+  ####
+  def move_robot(self, direction: tuple[int, int]):
+    new_x = self.robot[0] + direction[0]
+    new_y = self.robot[1] + direction[1]
+    #print(f'Moving robot to ({new_x}, {new_y})')
+
+    # Try to move the closest box to an empty space in the same
+    # direction as the robot is supposed to move, freeing space
+    # for the robot to move
+    if (new_x, new_y) in self.boxes:
+      potential_x = new_x
+      potential_y = new_y
+      while (potential_x, potential_y) in self.boxes:
+        potential_x += direction[0]
+        potential_y += direction[1]
+
+      # Can't move the box into an empty space therefore, we can't move the robot
+      if (potential_x, potential_y) in self.walls:
+        #print('...Can\'t move the box')
+        return
+      # Replace location of box into the space found
+      self.boxes.remove((new_x, new_y))
+      self.boxes.add((potential_x, potential_y))
+      #print(f'...Moved box to ({potential_x},{potential_y})')
+    if (new_x, new_y) in self.walls:
+      #print('..Location in walls')
+      return
+    if (new_x, new_y) in self.boxes:
+      #print('..Location still in boxes')
+      return
+    self.robot = (new_x, new_y)
+    #print(f'...Moved robot to ({self.robot[0]}, {self.robot[1]})')
+
 
 ####
 # readlines: reads input from file into lines of strings
@@ -63,8 +99,37 @@ def readlines(source):
 
 
 ####
+# Translate a move into a vector representing the direction of movement
+####
+def get_direction(move: str) -> tuple[int, int]:
+  if move == '<':
+    return (-1, 0)
+  elif move == '>':
+    return (1, 0)
+  elif move == '^':
+    return (0, -1)
+  elif move == 'v':
+    return (0, 1)
+  raise ValueError(f'Invalid move: {move}')
+
+
+####
 # Main
 ####
-input = readlines('sample.txt')
+input = readlines('hint.txt')
 warehouse = Warehouse(input)
+
+instructions = list[str]()
+for line in input:
+  if line.startswith('#'):
+    continue
+  if len(line) == 0:
+    continue
+  instructions.append(line)
+warehouse.draw()
+
+for line in instructions:
+  for move in line:
+    direction = get_direction(move)
+    warehouse.move_robot(direction)
 warehouse.draw()
