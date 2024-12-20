7dc820ecb6becaeaae901861ce1cd2c73566aa64
shirlsgaw <72216457+shirlsgaw@users.noreply.github.com>
2024-12-20 01:02:25 +0000

    Track pattern lengths

--- advent19.py
+++ advent19.py
@@ -21,11 +21,19 @@ def readlines(source):
 input = readlines('sample.txt')
 
 available_patterns = None
+min_length = None
+patterns_dict = dict[int, list[str]]()
 desired_designs = list[str]()
 for line in input:
   tokens = line.split(',')
   if len(tokens) > 1:
     available_patterns = tokens
+    for pattern in available_patterns:
+      length = len(pattern)
+      if min_length is None or length < min_length:
+        min_length = length
+      matches = patterns_dict.get(length, list[str]())
+      matches.append(pattern)
   elif len(line) > 0:
     desired_designs.append(line)
 print(f'Available patterns: {available_patterns}')
